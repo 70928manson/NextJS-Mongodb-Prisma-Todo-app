2705db824109f97f7b6dde9b013a4b97b31d7711
Manson <manson972@yahoo.com>
2024-01-01 13:43:18 +0800

    update Form component

--- component/ui/Form.tsx
+++ component/ui/Form.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+"use client"
 
-const Form = () => {
+import React from 'react';
+import { useRef, ReactNode } from 'react';
+
+interface formProps {
+  children: ReactNode,
+  action: (formData: FormData) => Promise<void | boolean>,
+  className?: string,
+  onSubmit?: () => void,
+}
+
+const Form = ({ children, action, className, onSubmit }: formProps) => {
+  const ref = useRef<HTMLFormElement>(null);
+
   return (
-    <div>Form</div>
+    <form 
+      className={className}
+      onSubmit={onSubmit}
+      ref={ref}
+      action={async (formData) => {
+        await action(formData);
+        ref.current?.reset();
+      }}
+    >
+      { children }
+    </form>
   )
 }
 
