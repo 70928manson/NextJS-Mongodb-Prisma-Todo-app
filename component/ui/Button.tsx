6b674e843f5e6bf67c596b84fb32dc0e4a7845bd
Manson <manson972@yahoo.com>
2024-01-01 13:07:54 +0800

    update ui component

--- component/ui/Button.tsx
+++ component/ui/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Button = () => {
   return (
@@ -6,4 +6,4 @@ const Button = () => {
   )
 }
 
-export default Button
+export default Button;
--- component/ui/Input.tsx
+++ component/ui/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Input = () => {
   return (
@@ -6,4 +6,4 @@ const Input = () => {
   )
 }
 
-export default Input
+export default Input;
